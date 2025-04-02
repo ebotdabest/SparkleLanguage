416faa8d7e505b31829d12d097b424786d3bfe92
ebotdabest <ebotdabest@gmail.com>
2025-04-02 13:12:42 +0200

    More changes

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from lexer import lexer as l
 from parser import parser as p
+from pprint import pprint
 
 tokens = l.get_tokens("""
 var public nonlocal x = 5+6;
-var nonlocal b = 4*2/8;
+public var nonlocal b = 4*2/8;
+const sanyi = 69;
 print(x+b);
 print("fuck you", "twice");
 """)
 
-print(p.build_ast(tokens))
+pprint(p.build_ast(tokens), indent=2)
--- parser/parser.py
+++ parser/parser.py
@@ -1,5 +1,6 @@
-from typing import List
+from typing import List, Dict
 from lexer.token import VARIABLE_MODIFIERS, VARIABLE_DECLARTOR, MUTABLE_DECLATOR, VARIABLE_SHIT
+
 
 def get_segments(tokens: List[str]):
     segments = []
@@ -13,9 +14,18 @@ def get_segments(tokens: List[str]):
 
     return segments   
 
+def value_breakdown(value: str) -> Dict:
+    breakdown = {
+        "type": "unknown",
+        "values:": []
+    }
+    if (value.startswith('"') and value.endswith('"')) or (value.startswith("'") and value.endswith("'")):
+        breakdown["type"] = "str_declr"
+
+
 def build_ast(tokens: List[str]):
     segments = get_segments(tokens)
-
+    ast = []    
     for s in segments:
         if s[0] in VARIABLE_SHIT:
             i = 0
@@ -23,7 +33,19 @@ def build_ast(tokens: List[str]):
             while s[i] in VARIABLE_SHIT:
                 modifiers.append(s[i])
                 i += 1
-            print(s, modifiers)
+            name = s[len(modifiers)]
+            value = "".join([s[sp] for sp in range(len(modifiers) +2, len(s))])
 
+            ast.append({
+                "type": "var_declr",
+                "properties": {
+                    "modifiers": modifiers,
+                    "type": "var" if "var" in modifiers else "const",
+                    "value": {
+                        "raw": value
+                    },
+                    "name": name
+                }
+            })
 
-    return segments
+    return ast
